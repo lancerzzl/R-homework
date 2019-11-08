2652665265544df28b3e7d5fcbaf70f530e21884
lancerzzl <zzl793780096@outlook.com>
2019-11-08 23:09:13 +0000

    wwd3

--- WWD_3.R
+++ WWD_3.R
@@ -22,6 +22,35 @@
   group.4 <- cutree(hd,4)
   table(animals$Species,group.4)
 }
+
+## build a function to conculate the max carvature
+{
+  curv <- function(x,y){
+    ta <- sqrt((x[2]-x[1])^2+(y[2]-y[1])^2)
+    tb <- sqrt((x[3]-x[2])^2+(y[3]-y[2])^2)
+    M_matrix <- matrix(c(1,ta,ta^2,1,0,0,1,tb,tb^2),3,3,byrow=TRUE)
+    M_inverse <- solve(M_matrix)
+    a <- M_inverse %*% x
+    b <- M_inverse %*% y
+    curvature <- 2*(a[3]*b[2]-a[2]*b[3])/sqrt(a[2]^2+b[2]^2)
+    return(curvature)
+  }
+  max.curv <- function(x,y) {
+      I <- c(2:(length(x)-1))
+      cur <- I
+    for (i in I){
+      s.x <- c(x[i-1],x[i],x[i+1])
+      s.y <- c(y[i-1],y[i],y[i+1])
+      cur[i] <- curv(s.x,s.y)
+    }
+      cur <- abs(cur)
+      max <- max(cur)
+      n <- which.max(cur)+1
+      result <- list(max,n,cur) 
+      names(result) <-c("max value","number","curature")
+      return(result)
+  }  
+}
 ## Q2 
 {
   animals_df <- cbind(animals["Weight"],animals["Height"])
@@ -32,21 +61,29 @@
   }
   size = c(2:10)
   clk <- sapply(size,tot.withinss,df = animals_df)
+  max.curv(size,clk)
   plot(size,clk)
-  line(3)
+  abline(v=3)
 }
+
+
+
+
 ##Q3
 {
-  ctot.withinss <- function(number,df,method="manhattan"){
-    c2 <- cclut(df,number,method=method)
-    return(c2$tot.withinss)
+  ctot.withinss <- function(number,df,dist="manhattan"){
+    df <- as.matrix(df)
+    c2 <- cclust(df,number,dist = dist)
+    return(sum(c2$withinss))
   }
-  cclk <- sapply(size,tot.withinss,df = animals_df)
+  cclk <- sapply(size,ctot.withinss,df = animals_df)
+  max.curv(size,cclk)
   plot(size,cclk)
+  abline(v=3)
   x <- cbind(clk,cclk)
 }
 
-maxcurv(cclk[1:2])
+
 
 
 
